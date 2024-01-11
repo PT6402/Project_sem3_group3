94a5d296f3cd5f6e33526868b92a3bac95ec2931
HieutranEproject <116696372+HieutranEproject@users.noreply.github.com>
2024-01-11 11:45:22 +0700

    Delete eProject Sem03_T1.2208M2_Nexus Service Marketing System_Group03_Final Submit/02. Developments/2.2. Source Code/2.2.1. Client directory

--- eProject Sem03_T1.2208M2_Nexus Service Marketing System_Group03_Final Submit/02. Developments/2.2. Source Code/2.2.1. Client/src/components/pages/private/admin/Users/Customers/Detail/index.jsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function Detail_customer() {
-  const { id: id } = useParams();
-  return <div>customers : {id}</div>;
-}
--- eProject Sem03_T1.2208M2_Nexus Service Marketing System_Group03_Final Submit/02. Developments/2.2. Source Code/2.2.1. Client/src/context/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as store } from "./store";
-export { default as toastSlice } from "./toastSlice";
-export { default as userSlice } from "./userSlice";
-export { default as privateUISlice } from "./privateUISlice";
-export { default as modalType } from "./modalSlice";
